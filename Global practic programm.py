64160d0b4b99cd294af0400e30394baafb57ae9e
BBR14910 <Zhirkovsg755@gmail.com>
2024-09-30 13:23:52 +0300

    Сделать расчет массы

--- Global practic programm.py
+++ Global practic programm.py
@@ -3,10 +3,14 @@ import sys
 print("Введите что-нибудь")
 choose_programm = 0
 for i in sys.stdin:
-    choose_programm = int(input("Введите целочисленное число для выбора программы (1 - найти скорость)"))
+    choose_programm = int(input("Введите целочисленное число для выбора программы (1 - найти скорость; 2 - найти массу)"))
     if i == 'q\n':
         exit(0)
     if choose_programm == 1:
         long = float(input('Введите расстояние в км'))
         time = float(input('Введите время в часах'))
-        print("Скорость =", long/time)
+        print("Скорость =", long/time, 'м/с')
+    elif choose_programm == 2:
+        Force = float(input("Введите силу в Н"))
+        acceleration = float(input("Введите ускорение в м/с^2"))
+        print("Масса =", Force/acceleration, "кг")
