e02ba107818efba88c33e05deab032325c1e5dcf
BBR14910 <Zhirkovsg755@gmail.com>
2024-09-30 13:42:21 +0300

    Сделать расчет кинетической энергии

--- Global practic programm.py
+++ Global practic programm.py
@@ -5,7 +5,7 @@ choose_programm = 0
 for i in sys.stdin:
     choose_programm = int(input("Введите целочисленное число для выбора программы (1 - найти скорость; "
                                 "2 - найти массу; 3 -перевести фаренгейты в цельсии;"
-                                "4 - найти работы)"))
+                                "4 - найти работы; 5 - найти кинетическую энергию)"))
 
     if i == 'q\n':
         exit(0)
@@ -28,5 +28,10 @@ for i in sys.stdin:
         Force = float(input("Введите силу в Н"))
         long = float(input('Введите расстояние в м'))
         print("Работа = ", Force * long, ' Дж')
+
+    elif choose_programm == 5:
+        mass = float(input("Введите массу в кг"))
+        speed = float(input('Введите скосроть в м/с'))
+        print("Кинетическая энергия = ", mass * speed**2/2, ' Дж')
 
     print("Введите что-нибудь")
