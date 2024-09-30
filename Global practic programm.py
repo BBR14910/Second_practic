7ea566ee189e45aac061ca44d1bb7c58f7e37175
BBR14910 <Zhirkovsg755@gmail.com>
2024-09-30 13:31:18 +0300

    Сделать расчет работы

--- Global practic programm.py
+++ Global practic programm.py
@@ -3,7 +3,9 @@ import sys
 print("Введите что-нибудь")
 choose_programm = 0
 for i in sys.stdin:
-    choose_programm = int(input("Введите целочисленное число для выбора программы (1 - найти скорость; 2 - найти массу; 3 -перевести фаренгейты в цельсии)"))
+    choose_programm = int(input("Введите целочисленное число для выбора программы (1 - найти скорость; "
+                                "2 - найти массу; 3 -перевести фаренгейты в цельсии;"
+                                "4 - найти работы)"))
 
     if i == 'q\n':
         exit(0)
@@ -21,3 +23,10 @@ for i in sys.stdin:
     elif choose_programm == 3:
         Temperature_Farengeyt = float(input("Введите температуру в фаренгейтах"))
         print("Температура в цельсия = ", (Temperature_Farengeyt - 32)*5/9)
+
+    elif choose_programm == 4:
+        Force = float(input("Введите силу в Н"))
+        long = float(input('Введите расстояние в м'))
+        print("Работа = ", Force * long, ' Дж')
+
+    print("Введите что-нибудь")
