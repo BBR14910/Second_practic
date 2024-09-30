f4b3a16660f58ca53250244836dbc089be90e06c
BBR14910 <Zhirkovsg755@gmail.com>
2024-09-30 13:27:45 +0300

    Сделать расчет перевода фаренгейты в цельсии

--- Global practic programm.py
+++ Global practic programm.py
@@ -3,14 +3,21 @@ import sys
 print("Введите что-нибудь")
 choose_programm = 0
 for i in sys.stdin:
-    choose_programm = int(input("Введите целочисленное число для выбора программы (1 - найти скорость; 2 - найти массу)"))
+    choose_programm = int(input("Введите целочисленное число для выбора программы (1 - найти скорость; 2 - найти массу; 3 -перевести фаренгейты в цельсии)"))
+
     if i == 'q\n':
         exit(0)
+
     if choose_programm == 1:
         long = float(input('Введите расстояние в км'))
         time = float(input('Введите время в часах'))
-        print("Скорость =", long/time, 'м/с')
+        print("Скорость = ", long/time, ' м/с')
+
     elif choose_programm == 2:
         Force = float(input("Введите силу в Н"))
         acceleration = float(input("Введите ускорение в м/с^2"))
-        print("Масса =", Force/acceleration, "кг")
+        print("Масса = ", Force/acceleration, " кг")
+
+    elif choose_programm == 3:
+        Temperature_Farengeyt = float(input("Введите температуру в фаренгейтах"))
+        print("Температура в цельсия = ", (Temperature_Farengeyt - 32)*5/9)
