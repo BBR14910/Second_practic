52e669cd24038e04d81c9ffce0d6d70e48571b2f
BBR14910 <Zhirkovsg755@gmail.com>
2024-09-30 13:11:57 +0300

    Сделать расчет скосроти

--- Global practic programm.py
+++ Global practic programm.py
@@ -1,6 +1,12 @@
 import sys
 
+print("Введите что-нибудь")
+choose_programm = 0
 for i in sys.stdin:
-    choose_programm = int(input("Введите целочисленное число для выбора программы"))
+    choose_programm = int(input("Введите целочисленное число для выбора программы (1 - найти скорость)"))
     if i == 'q\n':
         exit(0)
+    if choose_programm == 1:
+        long = float(input('Введите расстояние в км'))
+        time = float(input('Введите время в часах'))
+        print("Скорость =", long/time)
